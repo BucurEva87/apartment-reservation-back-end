775860e02af59fa73660002dfa71dc74603eac3e
BucurEva87 <bucur.eva87@gmail.com>
2023-04-10 15:21:17 +0300

    Modify code to abide linter rules

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -6,7 +6,7 @@ class Apartment < ApplicationRecord
   validates :description, presence: true
   validate :validate_photo_url
   validates :city, presence: true, length: { minimum: 2, maximum: 30, message: 'must be between 2 and 30 characters' }
-  validates :price, presence: true, numericality: { greater_than: 0, message: "must be greater than zero" }
+  validates :price, presence: true, numericality: { greater_than: 0, message: 'must be greater than zero' }
 
   private
 
@@ -14,9 +14,9 @@ class Apartment < ApplicationRecord
     return unless photo
 
     unless URI.parse(photo).is_a?(URI::HTTP) || URI.parse(photo).is_a?(URI::HTTPS)
-      errors.add(:photo, "must be a valid URL format")
+      errors.add(:photo, 'must be a valid URL format')
     end
   rescue URI::InvalidURIError
-    errors.add(:photo, "must be a valid URL format")
+    errors.add(:photo, 'must be a valid URL format')
   end
 end
