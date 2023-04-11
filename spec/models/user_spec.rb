72bc0c24c8dc5037a527a9f068ec47148dd06a4f
BucurEva87 <bucur.eva87@gmail.com>
2023-04-11 15:48:27 +0300

    Add tests for User model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_length_of(:name).is_at_least(2).is_at_most(30) }
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email).case_insensitive }
+    it { should allow_value('huckleberry.finn@marktwain.com').for(:email) }
+    it { should_not allow_value('tom.sawyer@marktwain').for(:email) }
+    it { should validate_inclusion_of(:role).in_array(['user', 'admin']) }
+  end
+
+  describe 'associations' do
+    it { should have_many(:reservations).dependent(:destroy) }
+    it { should have_many(:apartments).through(:reservations) }
+  end
+
+  describe '.authenticate!' do
+    user = User.create(name: 'TestUser', email: 'test.user@gmail.com', password: '123456')
+
+    it 'returns the user when valid email and password are provided' do
+      expect(User.authenticate!(user.email, user.password)).to eq(user)
+    end
+
+    it 'returns nil when invalid email is provided' do
+      expect(User.authenticate!('invalidemail@marktwain.com', user.email)).to be_nil
+    end
+
+    it 'returns nil when invalid password is provided' do
+      expect(User.authenticate!(user.email, 'invalidpassword')).to be_nil
+    end
+  end
+end
