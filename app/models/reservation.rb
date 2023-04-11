c9ff0090001f6c83e0b860dd88d11e432ae4a445
BucurEva87 <bucur.eva87@gmail.com>
2023-04-11 18:03:20 +0300

    Add validations for start_date, end_date, apartment, user, apartment already reserved, chronology of dates

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,14 +2,27 @@ class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :apartment
 
+  validates :user, presence: true
+  validates :apartment, presence: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validate :start_date_before_end_date
   validate :apartment_not_reserved, on: :create
 
   private
 
+  def start_date_before_end_date
+    if start_date && end_date && start_date >= end_date
+      errors.add(:start_date, "must be before end date")
+    end
+  end
+
   def apartment_not_reserved
-    if apartment.reservations.exists?(
-        "end_date >= ? AND start_date <= ?", 
-        start_date, end_date)
+    return unless apartment
+
+    if apartment.reservations.exists?([
+      "end_date >= ? AND start_date <= ?", 
+      start_date, end_date])
       errors.add(:apartment, "is already reserved for this period")
     end
   end
