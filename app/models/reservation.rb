c3523bb3fe44f53573855fc0e006abcbd960bed4
BucurEva87 <bucur.eva87@gmail.com>
2023-04-11 17:51:36 +0300

    Add reservation overlap validation

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,16 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :apartment
+
+  validate :apartment_not_reserved, on: :create
+
+  private
+
+  def apartment_not_reserved
+    if apartment.reservations.exists?(
+        "end_date >= ? AND start_date <= ?", 
+        start_date, end_date)
+      errors.add(:apartment, "is already reserved for this period")
+    end
+  end
 end
