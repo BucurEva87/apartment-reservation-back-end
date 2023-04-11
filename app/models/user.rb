7f5b0fd5797f26ac974990b163745e816b82937f
BucurEva87 <bucur.eva87@gmail.com>
2023-04-11 15:42:09 +0300

    Modify the regular expression for validating email

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,11 @@ class User < ApplicationRecord
   has_many :reservations, dependent: :destroy
   has_many :apartments, through: :reservations
 
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+
   validates :name, presence: true, length: { minimum: 2, maximum: 30 }
   validates :email, presence: true,
-                    format: { with: URI::MailTo::EMAIL_REGEXP },
+                    format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   validates :role, inclusion: { in: %w[user admin] }
 
